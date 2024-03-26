9acb6780c4e2f1adabed84464f8a14af314fa4fe
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2024-03-26 21:17:38 +0100

    second commit

--- boardgame/views.py
+++ boardgame/views.py
@@ -1,31 +1,59 @@
-from pathlib import Path
-from django.http import HttpResponseRedirect
+from __future__ import annotations
 from django.shortcuts import render
-from django import forms
 from django.urls import reverse
-
+from dataclasses import dataclass, field
 import json
-from . import util
-from django import forms
 
+@dataclass
+class Bücherhallen:
+    availability :str
+    count: str
+    current_item_state: str
+    category:str
+
+
+
+@dataclass
+class Game:
+    name: str 
+    rank: int
+    image: str
+    ebay_kleinanzeigen: EbayKleinanzeigen  = None
+    bücherhallen: Bücherhallen  = None
+
+@dataclass
+class EbayKleinanzeigen:
+    link:str
+    price:int
+    description:str
+    location:str
+    date:str
 
 
 def index(request):
-    games_list: list[util.Game] = util.get_top_100_games()
+    games_list: list[Game] = []
+
+    with open("boardgame/bgg_top_100.json") as f:
+        bgg_top_100 = json.load(f)
+
     with open("boardgame/ebay_kleinanzeigen_dict.json") as f:
         ebay_kleinanzeigen = json.load(f)
     
-    for game in games_list:
+    with open("boardgame/bücherhallen.json") as f:
+        bücherhallen = json.load(f)
 
+
+    for game in bgg_top_100:
+        game = Game(**game)
         ebay_entry = ebay_kleinanzeigen.get(game.name)
         if ebay_entry:
-            game.ebay_kleinanzeigen = util.EbayKleinanzeigen(link=ebay_entry["link"], 
-                              price=ebay_entry["price"], 
-                              description=ebay_entry["description"],
-                              location=ebay_entry["location"],
-                              date=ebay_entry["date"])
+            game.ebay_kleinanzeigen = EbayKleinanzeigen(**ebay_entry)
+        
+        bücherhallen_entry = bücherhallen.get(game.name)
+        if bücherhallen_entry:
+            game.bücherhallen = Bücherhallen(**bücherhallen_entry)
 
-
+        games_list.append(game)
     return render(request, "boardgame/index.html", {
             "entries": games_list
         })
